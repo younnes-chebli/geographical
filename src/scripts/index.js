3b070398560c7a075b7795e37aeb57cbba18ac86
Younnes Chebli <younnesc@gmail.com>
2022-11-11 11:55:15 +0100

    added getCoordinates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,37 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-const MAPBOX_KEY = process.env.MAPBOX_KEY;
+import * as dotenv from "dotenv"
+dotenv.config()
+const MAPBOX_KEY = process.env.MAPBOX_KEY
 
-const addressForm = document.getElementById("address-form");
+const addressForm = document.getElementById("address-form")
 
 const getCoordinates = async (address) => {
     try {
-        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?types=address&access_token=${MAPBOX_KEY}`);
-        const data = await response.json();
-        console.log(data);
+        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?types=address&access_token=${MAPBOX_KEY}`)
+        const data = await response.json()
+        const coordianates = data.features[0].center
+        return coordianates
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
-};
+}
+
+const isset = (string) => {
+    return string != ""
+}
+
+const getMap = async (lon, lat) => {
+
+}
 
 addressForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    const address = data.get("address");
-    const coordinates = await getCoordinates(address);
-});
+    e.preventDefault()
+    const data = new FormData(e.target)
+    const address = data.get("address")
+    if(isset(address)) {
+        const coordinates = await getCoordinates(address)
+        console.log(coordinates)
+        const lon = coordinates[0]
+        const lat = coordinates[1]
+        const map = await getMap(lon, lat)
+    }
+})
